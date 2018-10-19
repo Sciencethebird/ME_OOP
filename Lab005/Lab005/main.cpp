67ee5de12b09f9a2cd9672ecdc4e1600bf017aec
sciencethebird <sciencethebird@gmail.com>
2018-10-19 09:41:04 +0800

    Update Lab005/Lab005/main.cpp

---
+++ Lab005/Lab005/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab005
+//
+//  Created by Sciencethebird on 2018/10/19.
+//  Copyright © 2018年 Sciencethebird. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
