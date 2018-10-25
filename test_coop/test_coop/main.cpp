8886343972b42f3484fe9def97b40fdeca2df843
sciencethebird <sciencethebird@gmail.com>
2018-10-25 12:19:54 +0800

    Update test_coop/test_coop/main.cpp

---
+++ test_coop/test_coop/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test_coop
+//
+//  Created by Sciencethebird on 2018/10/25.
+//  Copyright © 2018年 Sciencethebird. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
